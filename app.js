9d6993bcd52e2616930a14db39897495e607de5b
Juno Han <junhohan2@gmail.com>
2021-10-02 05:25:51 -0700

    refactored

--- app.js
+++ app.js
@@ -1,65 +1,40 @@
-let count = 0;
-
 const Gameboard = (() => {
-  const boardData = [[], [], []];
+  const BOARD_DATA = [[], [], []];
+
   const createNewBoard = () => {
     const $body = document.body;
     const $gameboard = document.createElement("div");
+    const $result = document.createElement("div");
     $gameboard.classList.add("gameboard");
     for (let i = 0; i < 3; i++) {
-      const $div = document.createElement("div");
-      if (i === 0) {
-        $div.classList.add("l", "cell", "top", `${i}`);
-      } else if (i === 1) {
-        $div.classList.add("m", "cell", "top", `${i}`);
-      } else {
-        $div.classList.add("r", "cell", "top", `${i}`);
+      for (let j = 0; j < 3; j++) {
+        const $div = document.createElement("div");
+        $div.classList.add("cell", `${i}${j}`);
+        $gameboard.append($div);
       }
-      $gameboard.append($div);
-    }
-    for (let i = 0; i < 3; i++) {
-      const $div = document.createElement("div");
-      if (i === 0) {
-        $div.classList.add("l", "cell", "mid", `${i}`);
-      } else if (i === 1) {
-        $div.classList.add("m", "cell", "mid", `${i}`);
-      } else {
-        $div.classList.add("r", "cell", "mid", `${i}`);
-      }
-      $gameboard.append($div);
-    }
-    for (let i = 0; i < 3; i++) {
-      const $div = document.createElement("div");
-      if (i === 0) {
-        $div.classList.add("l", "cell", "bot", `${i}`);
-      } else if (i === 1) {
-        $div.classList.add("m", "cell", "bot", `${i}`);
-      } else {
-        $div.classList.add("r", "cell", "bot", `${i}`);
-      }
-      $gameboard.append($div);
     }
     $body.append($gameboard);
+    $body.append($result);
   };
 
-  return { createNewBoard, boardData };
+  return { createNewBoard, BOARD_DATA };
 })();
 
 const player = (symbol) => {
   const $grids = Array.from(document.querySelectorAll(".cell"));
-  const playerSymbol = symbol;
-  const computerSymbol = "X";
+  const PLAYER_SYMBOL = symbol;
+  const COMPUTER_SYMBOL = "X";
   const getSymbol = (symbol) => symbol;
+  const makeRandomNum = () => Math.floor(Math.random() * 3);
+  let COMP_COL = makeRandomNum();
+  let COMP_ROW = makeRandomNum();
+  const { BOARD_DATA } = Gameboard;
 
-  const computerMakeRandomNum = () => Math.floor(Math.random() * 3);
-
-  const compMove = () => {
-    let firstIndex = computerMakeRandomNum();
-    let secondIndex = computerMakeRandomNum();
-
-    while (Gameboard.boardData[firstIndex][secondIndex]) {
-      firstIndex = computerMakeRandomNum();
-      secondIndex = computerMakeRandomNum();
+  function compMove() {
+    while (BOARD_DATA[COMP_ROW][COMP_COL]) {
+      COMP_ROW = makeRandomNum();
+      COMP_COL = makeRandomNum();
+      let count = 0;
       $grids.forEach((grid) => {
         grid.textContent ? count++ : 0;
       });
@@ -69,103 +44,61 @@ const player = (symbol) => {
       count = 0;
     }
 
-    Gameboard.boardData[firstIndex][secondIndex] = getSymbol(computerSymbol);
+    BOARD_DATA[COMP_ROW][COMP_COL] = getSymbol(COMPUTER_SYMBOL);
 
-    if (firstIndex === 0) {
-      if (secondIndex === 0) {
-        $grids[0].textContent = getSymbol(computerSymbol);
-      }
-      if (secondIndex === 1) {
-        $grids[1].textContent = getSymbol(computerSymbol);
-      }
-      if (secondIndex === 2) {
-        $grids[2].textContent = getSymbol(computerSymbol);
-      }
-    }
-    if (firstIndex === 1) {
-      if (secondIndex === 0) {
-        $grids[3].textContent = getSymbol(computerSymbol);
-      }
-      if (secondIndex === 1) {
-        $grids[4].textContent = getSymbol(computerSymbol);
-      }
-      if (secondIndex === 2) {
-        $grids[5].textContent = getSymbol(computerSymbol);
-      }
-    }
-    if (firstIndex === 2) {
-      if (secondIndex === 0) {
-        $grids[6].textContent = getSymbol(computerSymbol);
-      }
-      if (secondIndex === 1) {
-        $grids[7].textContent = getSymbol(computerSymbol);
-      }
-      if (secondIndex === 2) {
-        $grids[8].textContent = getSymbol(computerSymbol);
-      }
-    }
-  };
-
-  function move(e) {
-    const index = e.target;
-    if (this.textContent) return;
-    this.textContent = getSymbol(playerSymbol);
-    this.classList.add(getSymbol(playerSymbol));
-
-    boardDB(index);
-    setTimeout(compMove, 1000);
-  }
-
-  const boardDB = (index) => {
-    if (index.classList.contains("top")) {
-      if (index.classList.contains("l")) {
-        Gameboard.boardData[0][0] = getSymbol(playerSymbol);
-      } else if (index.classList.contains("m")) {
-        Gameboard.boardData[0][1] = getSymbol(playerSymbol);
-      } else {
-        Gameboard.boardData[0][2] = getSymbol(playerSymbol);
-      }
-    }
-    if (index.classList.contains("mid")) {
-      if (index.classList.contains("l")) {
-        Gameboard.boardData[1][0] = getSymbol(playerSymbol);
-      } else if (index.classList.contains("m")) {
-        Gameboard.boardData[1][1] = getSymbol(playerSymbol);
-      } else {
-        Gameboard.boardData[1][2] = getSymbol(playerSymbol);
-      }
-    }
-    if (index.classList.contains("bot")) {
-      if (index.classList.contains("l")) {
-        Gameboard.boardData[2][0] = getSymbol(playerSymbol);
-      } else if (index.classList.contains("m")) {
-        Gameboard.boardData[2][1] = getSymbol(playerSymbol);
-      } else {
-        Gameboard.boardData[2][2] = getSymbol(playerSymbol);
-      }
-    }
-  };
-
-  function draw() {
     $grids.forEach((grid) => {
-      if (grid.textContent === "") {
-        grid.addEventListener("click", move);
+      if (
+        !grid.textContent &&
+        grid.classList.contains(`${COMP_ROW}${COMP_COL}`)
+      ) {
+        grid.textContent = getSymbol(COMPUTER_SYMBOL);
       }
     });
   }
-  return { draw };
+
+  const boardDB = (index) => {
+    for (let i = 0; i < 3; i++) {
+      for (let j = 0; j < 3; j++) {
+        if (index.classList.contains(`${i}${j}`)) {
+          BOARD_DATA[i][j] = getSymbol(PLAYER_SYMBOL);
+        }
+      }
+    }
+  };
+
+  function played(e) {
+    if (this.textContent) return;
+    this.textContent = getSymbol(PLAYER_SYMBOL);
+    this.classList.add(getSymbol(PLAYER_SYMBOL));
+    boardDB(this);
+    compMove.apply(this);
+    // checkWinner();
+  }
+  function start() {
+    $grids.forEach((grid) => grid.addEventListener("click", played));
+  }
+
+  return { start, played };
 };
 
-const playGame = (() => {
+const game = (() => {
   let gameOver = false;
+  const AI_LEVEL = ["easy", "unbeatable"];
+
+  const checkWinner = (playerSym, computerSym) => {};
+
   Gameboard.createNewBoard();
   const user = player("O");
-  user.draw()
+  user.start();
+  //
+
 
 
   // Check gameover
   // every grid is marked && winningCondition
+
   // const winningCondition() {
-  //     if(Gameboard.boardData[0][0] === player1 )
+  //     if(Gameboard.BOARD_DATA[0][0] === player1 )
   // }
+  return { checkWinner };
 })();
